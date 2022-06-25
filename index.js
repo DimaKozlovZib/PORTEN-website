cf44e66f3ef4ab44a5a54bb40c39c291d7adb220
Dima <dimaziboc@gmail.com>
2022-06-25 20:19:18 +0300

    исправлено меню в мобильной версии сайта

--- index.js
+++ index.js
@@ -2,11 +2,14 @@
 document.querySelector("#burger-memu").onclick = () => {
     let classButton = document.querySelector("#burger-memu").classList;
     let classMenu = document.querySelector(".header").classList;
+    let body = document.querySelector("body").classList
     if (!classMenu.contains("active")) {
         classButton.add("active");
         classMenu.add("active")
+        body.add("mobile-menu-acctive")
     } else {
         classButton.remove("active");
         classMenu.remove("active")
+        body.remove("mobile-menu-acctive")
     }
 }
